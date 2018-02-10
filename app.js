df372b5e0ee738ace5dfacbb54ea0aeca1b293b6
Angel Antezana <ghehahanghel@gmail.com>
2018-02-09 20:57:00 -0400

    Routing en express

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ app.get('/',function (request, response) {
   response.send('hello world');
 });
 
+app.get('/usuarios',function (request, response) {
+  response.send('accediendo a usuarios con el metodo get');
+});
+
+app.post('/usuarios',function (request, response) {
+  response.send('accediendo a usuarios con el metodo post');
+});
+
+app.put('/usuarios',function (request, response) {
+  response.send('accediendo a usuarios con el metodo put');
+});
+
+app.delete('/usuarios',function (request, response) {
+  response.send('accediendo a usuarios con el metodo delete');
+});
+
 app.listen(3000,function () {
   console.log('corriendo en el puerto 3000');
 });
