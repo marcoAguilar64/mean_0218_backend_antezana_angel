a2f8a3ebb89dc0cbffad8f143e087fe1651acfaf
Angel Antezana <ghehahanghel@gmail.com>
2018-02-14 21:10:26 -0400

    Instalando y configurando mongoose

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var app = express();
+
+mongoose.connect('mongodb://localhost/exampledb');
+
+mongoose.connection.on('error',function () {
+  console.log('error..............');
+});
+
+mongoose.connection.once('open', function () {
+  console.log('We are conected to mongodb ;)');
+});
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
