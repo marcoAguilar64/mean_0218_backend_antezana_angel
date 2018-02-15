585f3abf649927336f0945cb37f839febff9639a
Angel Antezana <ghehahanghel@gmail.com>
2018-02-14 21:40:13 -0400

    Creando modelos version beta

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var userModel = require('../models/user.model');
 
 router.get('/', function (request, response) {
   response.send('accediendo a usuarios con el metodo get');
 });
 
 router.post('/', function (request, response) {
-  response.send('accediendo a usuarios con el metodo post');
-  console.log('log post: ',request.body);
+userModel.create(request.body, function (err, user) {
+  
+});
 });
 
 router.put('/', function (request, response) {
   response.send('accediendo a usuarios con el metodo put');
-  console.log('log put: ',request.body);
+  console.log('log put: ', request.body);
 });
 
 router.delete('/', function (request, response) {
