1be4b13cca7a7dae5deff63b3742667cc38f02f4
Angel Antezana <ghehahanghel@gmail.com>
2018-02-15 20:23:12 -0400

    Implementado metodo post

--- routes/users.route.js
+++ routes/users.route.js
@@ -3,13 +3,28 @@ var router = express.Router();
 var userModel = require('../models/user.model');
 
 router.get('/', function (request, response) {
-  response.send('accediendo a usuarios con el metodo get');
+  response.send({
+    message: 'accediendo a usuarios con el metodo get'
+  });
 });
 
 router.post('/', function (request, response) {
-userModel.create(request.body, function (err, user) {
-  
-});
+  var newUser = new userModel(request.body);
+  newUser.save(function (err, userCreated) {
+    if (err) {
+      return response.status(500)
+        .send({
+          message: 'There was a problem registering ther user',
+          error: err
+        });
+    } else {
+      response.send({
+        message: 'A new user has been created',
+        data: userCreated
+      });
+    }
+
+  });
 });
 
 router.put('/', function (request, response) {
