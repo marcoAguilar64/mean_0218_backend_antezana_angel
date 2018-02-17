86bb1f7b952e6b99653d83bf208c75ad6a1c668e
Angel Antezana <ghehahanghel@gmail.com>
2018-02-16 20:39:16 -0400

    Validacion basica de un modelo

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,44 @@
 var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
-  name: String
+  name: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique:true,
+    minlength: 5,
+    maxlength:10,
+    trim:true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique:true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    default: 'http://fotouser.miarroba.st/68731257/300/mr-anonimo.jpg'
+  },
+  type: {
+    type: String,
+    enum:['USER','ADM'],
+    default: 'USER'
+  },
+  deleted: {
+    type: Boolean,
+    default: false
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
