dcc5b61d93c40e70cb207cd0e7910ed2d21d613d
Angel Antezana Flores <ghehahanghel@gmail.com>
2018-02-19 20:30:01 -0400

    Configuraion manual del cors

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ var mongoose = require('mongoose');
 
 var app = express();
 
+app.use(function (request, response, next) {
+  response.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  response.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETED');
+  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content_type, Accept');
+  next();
+});
+
+
+
 mongoose.connect('mongodb://localhost/exampledb');
 
-mongoose.connection.on('error',function () {
+mongoose.connection.on('error', function () {
   console.log('error..............');
 });
 
@@ -24,12 +33,12 @@ app.use(bodyParser.json());
 
 var usersRoute = require('./routes/users.route');
 
-app.get('/',function (request, response) {
+app.get('/', function (request, response) {
   response.send('hello world');
 });
 
 app.use('/users', usersRoute);
 
-app.listen(3000,function () {
+app.listen(3000, function () {
   console.log('corriendo en el puerto 3000');
 });
