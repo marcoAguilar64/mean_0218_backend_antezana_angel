6853307a64d2112f87647d751a160902c962f5f3
Angel Antezana Flores <ghehahanghel@gmail.com>
2018-02-21 17:15:34 -0400

    Creando el modelo para article

---
+++ models/article.model.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+
+var articleSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5,
+    maxlength: 70,
+    trim: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+});
+
+articleSchema.methods.getDtoArticle = function () {
+  var articleDTO = {
+    _id: this._id,
+    title: this.title,
+    content: this.content,
+    owner: this.owner
+  };
+  return articleDTO;
+};
+
+module.exports = mongoose.model('Article', articleSchema);
